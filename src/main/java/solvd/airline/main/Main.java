58a4f1da1ac719a5a7a01eca34cafd0acd663e83
Sheetal Garg <sheetal.garg05@gmail.com>
2023-07-07 12:29:58 -0400

    initial merge commit

--- src/main/java/solvd/airline/main/Main.java
+++ src/main/java/solvd/airline/main/Main.java
@@ -1,13 +1,14 @@
 package solvd.airline.main;
 
 import solvd.airline.menu.MainMenu;
+import solvd.airline.menu.RouteSelectionMenu;
 
 import java.io.IOException;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
-		MainMenu.launch();
+		RouteSelectionMenu.launch();
 
 	}
 }
