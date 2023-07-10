980b4285731751a926aeb8085c8778ef71250ad3
Sheetal Garg <sheetal.garg05@gmail.com>
2023-07-10 15:31:45 -0400

    added local requestInt in MainMenu

--- src/main/java/solvd/airline/menu/MainMenu.java
+++ src/main/java/solvd/airline/menu/MainMenu.java
@@ -5,23 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import javax.xml.bind.JAXBException;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import solvd.airline.dataaccess.model.AirlineRoute.AirLineRoute;
 
 import solvd.airline.dataaccess.model.AirlineRoute.AirLineRoutes;
-import solvd.airline.dataaccess.model.Location.Location;
 import solvd.airline.dataaccess.service.AirLineRouteMybatisService;
-import solvd.airline.dataaccess.service.LocationMyBatisService;
 import solvd.airline.output.JsonParser;
 import solvd.airline.output.XmlParser;
 
 public class MainMenu {
 	
 	private static final Logger logger = LogManager.getLogger(MainMenu.class);
+	private static Scanner scanner = new Scanner(System.in);
 	private static AirLineRouteMybatisService airLineRouteMybatisService = new AirLineRouteMybatisService();
 	private static List<AirLineRoute> airlineRouteList = new ArrayList<>();
 	
@@ -32,7 +29,7 @@ public class MainMenu {
 		logger.info("\n1. View All Routes");
 		logger.info("\n2. Select Itinerary");
 		logger.info("\n3. Exit");
-		int response = RouteSelectionMenu.requestInt("\n\nEnter input :");
+		int response = requestInt("\n\nEnter input :");
 
 		switch(response) {
 			case 1 : printAirlineRouteList();
@@ -56,46 +53,9 @@ public class MainMenu {
 		return airLineRouteMybatisService.getAllRoutes();
 	}
 	
-//	private static void checkRoutes() {
-//		try {
-//			// Print all the routes
-//			logger.info("\n--------Display--AlL--Routes--------------");
-//			airlineRouteList.forEach(System.out::println);
-//			System.out.println();
-//
-//			logger.info("\n--------Display--All--Locations-----------");
-//			locationList.forEach(System.out::println);
-//			System.out.println();
-//
-//			int originId = requestPrompt("\nPlease enter your current location id:");
-//			int destinationId = requestPrompt("\nPlease enter your destination location id:");
-//
-//			logger.info("\n-----------------------------------------");
-//			logger.info("Please choose your route:");
-//			logger.info("1. Fastest route");
-//			logger.info("2. Cheapest route");
-//			logger.info("\n-----------------------------------------");
-//
-//			int option = scanner.nextInt();
-//
-//			if (option == 1) {
-//				routeSelectionMenuHelper.getItineraryQueryResult(originId, destinationId);// replace after fastest route
-//			}
-//			else if (option == 2){
-//				routeSelectionMenuHelper.getItineraryQueryResult(originId, destinationId);// replace after cheapest route
-//			}
-//			else {
-//				logger.info("Invalid option, please enter 1 for fastest route or 2 for cheapest route.");
-//			}
-//		} catch (Exception e) {
-//			logger.error("An error occurred during route selection", e);
-//			logger.info("An error occurred, please try again.");
-//		}
-//	}
-//
-//	public static int requestPrompt(String prompt) {
-//		logger.info(prompt);
-//		int id = scanner.nextInt();
-//		return id;
-//	}
+	public static int requestInt(String prompt) {
+		logger.info(prompt);
+		int id = scanner.nextInt();
+		return id;
+	}
 }
